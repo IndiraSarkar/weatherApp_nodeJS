77086ab848b93530210027ba612e6b899c71e0f9
STS\indira.sarkar <indira.sarkar@sts.in>
2020-03-27 16:23:38 +0530

    added code to show temperature high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location. Try another search.', undefined);
         } else {
-            callback(undefined,  body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees fahrenheit. There is '+body.currently.precipProbability+'% chance of rain.');
+            callback(undefined,  body.daily.data[0].summary+' It is currently '+body.currently.temperature+' degrees fahrenheit. The high today is '+body.daily.data[0].temperatureHigh+' and the low is '+body.daily.data[0].temperatureLow+' There is '+body.currently.precipProbability+'% chance of rain.');
         }
     })
 }
